29ab274dbbf4ba2e82a63eb4e8d24ea21c33e96c
Kiril Spasov <spasov.kiril@gmail.com>
2023-01-12 10:13:03 +0200

    Add project files.

---
+++ Diff/FormDiff.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Diff
+{
+    partial class FormDiff
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCalculateDiff = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCalculateDiff
+            // 
+            this.BtnCalculateDiff.Location = new System.Drawing.Point(62, 58);
+            this.BtnCalculateDiff.Name = "BtnCalculateDiff";
+            this.BtnCalculateDiff.Size = new System.Drawing.Size(174, 58);
+            this.BtnCalculateDiff.TabIndex = 0;
+            this.BtnCalculateDiff.Text = "Calculate Difference";
+            this.BtnCalculateDiff.UseVisualStyleBackColor = true;
+            this.BtnCalculateDiff.Click += new System.EventHandler(this.BtnCalculateDiff_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(277, 58);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(458, 318);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormDiff
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnCalculateDiff);
+            this.Name = "FormDiff";
+            this.Text = "Difference Count";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCalculateDiff;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ Diff/FormDiff.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Diff
+{
+    public partial class FormDiff : Form
+    {
+        public FormDiff()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculateDiff_Click(object sender, EventArgs e)
+        {
+            string word1, word2;
+
+            string path = Application.StartupPath + @"\diff.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            bool finished = false;
+
+            int pairsCount = Convert.ToInt32(streamReader.ReadLine());
+
+            for (int i = 1; i <= pairsCount; i++)
+            {
+                word1 = streamReader.ReadLine();
+                word2 = streamReader.ReadLine();
+                TxtResult.Text += CalculateDifference(word1, word2) + Environment.NewLine;
+            }
+        }
+
+        private int CalculateDifference(string word1, string word2)
+        {
+            string character1, character2 = "";
+            int sameCount = 0;
+
+            for (int i = 0; i < word1.Length; i++)
+            {
+                character1 = word1.Substring(i, 1);
+
+                if (i < word2.Length)
+                {
+                    character2 = word2.Substring(i, 1);
+                }
+
+                if (character1 != character2)
+                {
+                    break;
+                }
+                else
+                {
+                    sameCount++;
+                }
+            }
+            return sameCount;
+        }
+    }
+}
